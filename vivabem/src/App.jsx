64991bc53c64b0b2e1905ba332c1768513e4701d
gabriel <jgsobralnunes@gmail.com>
2025-11-20 18:06:57 -0300

    senha gerada com sucesso, MÂSSSS esta no console

--- vivabem/src/App.jsx
+++ vivabem/src/App.jsx
@@ -4,11 +4,43 @@ import Header from './components/Header.jsx';
 import './components/Header.css';
 import SelecionarSenha from "./pages/selecionarSenha.jsx";
 
-export default function App() {
+// --- GERADOR DE SENHAS ---
+const contadores = {
+  SP: 0,
+  SG: 0,
+  SE: 0
+};
 
-  const handleSelecionar = (tipo) => {
-    console.log("Tipo selecionado:", tipo);
+function gerarSenha(tipoEscolhido) {
+  const hoje = new Date();
+
+  const YY = String(hoje.getFullYear()).slice(-2);
+  const MM = String(hoje.getMonth() + 1).padStart(2, "0");
+  const DD = String(hoje.getDate()).padStart(2, "0");
+
+  const tipoParaPP = {
+    prioritaria: "SP",
+    geral: "SG",
+    exame: "SE"
   };
+
+  const PP = tipoParaPP[tipoEscolhido];
+
+  // aumenta o contador do tipo
+  contadores[PP] += 1;
+
+  const SQ = String(contadores[PP]).padStart(2, "0");
+
+  return `${YY}${MM}${DD}-${PP}${SQ}`;
+}
+
+
+
+export default function App() {
+const handleSelecionar = (tipo) => {
+  const senha = gerarSenha(tipo);
+  console.log("Senha gerada:", senha);
+};
 
   return (
     <BrowserRouter>
